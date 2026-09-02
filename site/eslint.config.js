25315d9ec0f67eda72d0c55c0c761235f49ffa3e
JManion32 <jmanion32@gmail.com>
2026-09-01 23:39:22 -0400

    Fix lint

--- site/eslint.config.js
+++ site/eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import pluginVue from 'eslint-plugin-vue'
+import pluginVue from 'eslint-plugin-vue';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
@@ -8,11 +8,7 @@ export default defineConfig([
     globalIgnores(['dist']),
     {
         files: ['**/*.{ts,tsx}'],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            pluginVue.configs['flat/recommended'],
-        ],
+        extends: [js.configs.recommended, tseslint.configs.recommended, pluginVue.configs['flat/recommended']],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
