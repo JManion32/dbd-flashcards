58f9e6b28f31d0e09b47a73b1f97cddb63428edc
JManion32 <jmanion32@gmail.com>
2026-09-03 23:29:39 -0400

    Test deployment

--- site/src/components/Home.vue
+++ site/src/components/Home.vue
@@ -6,6 +6,7 @@ import '../styles/home.css';
     <div class="home-page-container">
         <h1 class="title-header">DBD Flashcards</h1>
         <p>An unofficial Dead by Daylight study tool!</p>
+        <p>TESTING TESTING</p>
     </div>
 </template>
 
